949bcf00ae353fa98595263936517c65057f54c8
e.rassadnikov <e.rassadnikov@rosvodokanal.ru>
2020-07-13 17:56:57 +0300

    Доработка демонстрационной формы

--- doc/js_forms/form_71.js
+++ doc/js_forms/form_71.js
@@ -16,9 +16,21 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Web-olap.  If not, see <https://www.gnu.org/licenses/>.*/
 $(document).ready(function(){
-$(".no_panel").on("before_load_data1", "#in_action_value", function(e){ 
-  var id_t=1;
-alert('Действие до загрузки данных');
-});
+    $(".no_panel").on("before_load_data1", "#in_action_value", function(e){ 
+        var id_t=1;
+        //alert('Действие до загрузки данных');
+    });
+    
+    $(".no_panel" ).on("after_build", "div.params_group[id=1]", function(e,params_group_clon) {
+        $(this).find('input#BD').val('2018-07-01');
+        $(this).find('input#ED').val('2018-07-10');
+        var sel_er=$(this).find('select#ER'),
+            sel_e=$(this).find('select#E');
+        $(sel_er).find('option').attr('selected','selected');
+        olap_param_upd_child(1,'ER'); 
+        $(sel_e).find('option').attr('selected','selected');
+        $(sel_er).multiselect('refresh');
+        $(sel_e).multiselect('refresh');
+    });
 
 });
